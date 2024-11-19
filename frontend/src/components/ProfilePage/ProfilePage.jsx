b928af88778727681f5e8af005556c69a1d985e9
satvikpathak007 <satvikpathak007@gmail.com>
2024-11-19 22:36:10 +0530

    touch

--- frontend/src/components/ProfilePage/ProfilePage.jsx
+++ frontend/src/components/ProfilePage/ProfilePage.jsx
@@ -7,7 +7,6 @@ import img4 from "./4.jpg";
 import img5 from "./5.jpg";
 import img6 from "./6.jpg";
 
-
 function ProfilePage() {
   const [profiles, setProfiles] = useState([]); // State for all user profiles
   const [searchQuery, setSearchQuery] = useState(""); // Search query state
